a65a328746b76c1eb42a800cef1a3edf8bfc8c16
Лобец Сергей Александрович <SA.Lobets.2711@fss.local>
2022-01-24 17:11:44 +1000

    Updates

---
+++ static/js/3.792ef21f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpnew-app"]=this["webpackJsonpnew-app"]||[]).push([[3],{289:function(n,s,t){},290:function(n,s,t){"use strict";t.r(s);var e=t(289),a=t.n(e),i=t(0);s.default=function(){return Object(i.jsx)("div",{className:a.a.main,children:"settings"})}}}]);
+//# sourceMappingURL=3.792ef21f.chunk.js.map
